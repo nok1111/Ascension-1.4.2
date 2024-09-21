ae2eaf5724f5fd3d9b2cb581cc04bf3d77c49cfc
Nokturno <35547755+nok1111@users.noreply.github.com>
2024-09-21 16:16:02 -0500

    Create pets_lib.lua

---
+++ data/lib/additional/pets_lib.lua
@@ -0,0 +1,225 @@
+egg2PetType = {
+    ["37934"] = 1,--loot,
+    ["37887"] = 2,--loot,
+    ["37900"] = 3,--loot,
+    ["37901"] = 4,
+    ["37813"] = 5, --rare loot
+    ["37941"] = 6, --fame
+	["37978"] = 7,--loot,
+	["37862"] = 8, --loot,
+	["37875"] = 9,
+	["37872"] = 10,--loot,
+	["37882"] = 11,--loot,
+	["37883"] = 12,--loot,
+	["37887"] = 13,
+	["37879"] = 14,
+	["37895"] = 15,--loot,
+	["37888"] = 16,--loot,
+	["37981"] = 17,--loot,
+	["37886"] = 18,
+	["37890"] = 19,	--loot,
+	["37946"] = 20, --loot,
+	["37942"] = 21,
+	["37943"] = 22, --loot,
+	["37864"] = 23, --loot,
+	["37863"] = 24,
+	["37869"] = 25,
+	["37877"] = 26,
+	["37873"] = 27,
+	["37874"] = 28,
+	["37973"] = 29,
+	["37971"] = 30, --loot, 
+	["37972"] = 31,
+	["37891"] = 32,
+	["37881"] = 33, --loot, 
+
+	["37940"] = 34, -- white owl --npc
+	["37976"] = 35, -- penguin -- dungeon loot
+	["37959"] = 36, -- terroc
+	["37961"] = 37, -- snail --fame
+	["37908"] = 38, -- purple chicken --loot
+	["37899"] = 39, -- mystic bby dragon
+	["37904"] = 40, -- baby rex
+	["37861"] = 41, -- baby prisma
+	["37912"] = 42, -- baby twin turtle --raid drop
+	["37925"] = 43, -- baby ice fenix
+	["37920"] = 44, -- baby fire fenix
+	["37968"] = 45, -- blood bug
+	["37954"] = 46, -- bunny -- quest chest
+	["37969"] = 47, -- dream slime
+	["37914"] = 48, -- bob 1
+	["37926"] = 49, -- bob 2
+	["37977"] = 50, -- baby crow --npc
+	["37938"] = 51, -- sheep --quest reward
+	["37911"] = 52, -- fairy
+	["37929"] = 53, -- old mummy --quest reward
+	["37927"] = 54, -- baby vector
+	["37947"] = 55, -- baby Frazzlemaw
+	["37906"] = 56, -- Darkin
+	["37917"] = 57, -- green ghost --npc
+	["37896"] = 58, -- small pidgeon --npc
+	["37936"] = 59, -- small dragon-fly --fame
+	["37922"] = 60, -- baby poodle	--npc
+	["37949"] = 61, -- jelly jelly --npc
+	["37935"] = 62, -- baby squid --npc
+	["37951"] = 63, -- night frog --npc
+	["37956"] = 64, -- gumateddy
+	["37923"] = 65, -- night butterfly --fame
+	["37984"] = 66, -- baby nightmare
+	["37945"] = 67, -- squirrel--loot
+	["37944"] = 68, -- seagul--npc
+	["37957"] = 69, -- firewind parrot--npc
+	["37948"] = 70, -- baby dworc
+	["37955"] = 71, -- flamingo --npc
+	["37980"] = 72, -- crab --npc
+	["37967"] = 73, -- baby eyeboh --npc
+	["37894"] = 74, -- skunk --loot
+	["37892"] = 75, -- badger --loot
+	["37933"] = 76, -- insectoid larve --loot
+	["37963"] = 77, -- scarab --loot
+	["37966"] = 78 -- bug --loot --fame
+
+
+
+	
+}
+
+petTypeToPetName = {
+    [1] = "white cat*",--loot,
+    [2] = "gray cat*",--loot,
+    [3] = "black cat*",--loot,
+    [4] = "golden cat*",
+    [5] = "orc",
+    [6] = "wasp*",--loot,
+	[7] = "dark slime*",--loot,
+	[8] = "Chicken*", --loot,
+	[9] = "baby elemental*",--loot, --npc
+	[10] = "aqua slime*",--loot,
+	[11] = "sand spider*",--loot,
+	[12] = "mouse*", --loot,
+	[13] = "turtle*",--FISHING
+	[14] = "rabit*",
+	[15] = "old dog*",--loot,
+	[16] = "snake*",--loot,
+	[17] = "deer*",--loot,
+	[18] = "bee queen*",
+	[19] = "ghost*",
+	[20] = "wolf cub*", --loot,
+	[21] = "goblin*",
+	[22] = "spectre*",--loot,
+	[23] = "black spider*", --loot,
+	[24] = "sheep*",
+	[25] = "air elemental*",
+	[26] = "the thing*",
+	[27] = "baby angel*",
+	[28] = "winged angel*",
+	[29] = "fox*",
+	[30] = "boar cub*", --loot, 
+	[31] = "baby*",
+	[32] = "furry*",
+	[33] = "bear cub*", --loot, 
+	[34] = "white owl*",
+	[35] = "penguin*",
+	[36] = "terroc*",
+	[37] = "snail*",
+	[38] = "purple chicken*",
+	[39] = "mystic baby dragon*",
+	[40] = "baby rex*",
+	[41] = "baby prisma*",
+	[42] = "baby twin turtle*",
+	[43] = "baby ice fenix*",
+	[44] = "baby fire fenix*",
+	[45] = "blood bug*",
+	[46] = "bunny*",
+	[47] = "dream slime*",
+	[48] = "bob 1*",
+	[49] = "bob 2*",
+	[50] = "baby crow*",
+	[51] = "sheep*",
+	[52] = "fairy*",
+	[53] = "old mummy*",
+	[54] = "baby vector*",
+	[55] = "baby Frazzlemaw*",
+	[56] = "darkin*",
+	[57] = "green ghost*",
+	[58] = "small pidgeon*",
+	[59] = "small dragon-fly*",
+	[60] = "baby poodle*",
+	[61] = "jelly jelly*",
+	[62] = "baby squid*",
+	[63] = "night frog*",
+	[64] = "gumateddy*",
+	[65] = "night butterfly*",
+	[66] = "baby nightmare*",
+	[67] = "squirrel*",
+	[68] = "seagul*",
+	[69] = "firewind parrot*",
+	[70] = "baby dworc*",
+	[71] = "flamingo*",
+	[72] = "crab*",
+	[73] = "baby eyeboh*",
+	[74] = "skunk*",
+	[75] = "badger*",
+	[76] = "insectoid larve*",
+	[77] = "scarab*",
+	[78] = "bug*"
+
+
+
+}
+
+petExpByLevel = {
+    [1] = 500,
+    [2] = 1500,
+    [3] = 3000,
+    [4] = 6200,
+    [5] = 9500,
+    [6] = 13000,
+    [7] = 15000,
+    [8] = 20000,
+    [9] = 30000,
+    [10] = 50000
+}
+
+function getPetMaxExpByLevel(level)
+    if level > 0 and level <= 10 then
+        return petExpByLevel[level]
+    else
+        return petExpByLevel[10] -- cap pet level at 10 if it goes beyond
+    end
+end
+
+-- pets.lua
+
+function addPetExp(master, exp)
+    if not master then
+        return false
+    end
+    
+    local currentExp = master:getStorageValue(STORAGEVALUE_PET_EXP)
+    local petLevel = master:getStorageValue(STORAGEVALUE_PET_LVL)
+
+    Player.setPetExp(master, exp)
+    
+    if currentExp >= getPetMaxExpByLevel(petLevel) then
+        addPetLevel(master, 1)
+    end
+    
+    return true
+end
+
+function addPetLevel(master, levelsToAdd)
+    if not master then
+        return false
+    end
+    
+    local currentLevel = master:getStorageValue(STORAGEVALUE_PET_LVL)
+    local newLevel = currentLevel + levelsToAdd
+
+    if newLevel > 10 then
+        newLevel = 10 -- Cap pet level at 10
+    end
+    
+    master:setStorageValue(STORAGEVALUE_PET_LVL, newLevel)
+    return true
+end
