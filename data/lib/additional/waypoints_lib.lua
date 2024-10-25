925e26bfefb7bda043d6ee41af3a8045425c3dc5
Nokturno <35547755+nok1111@users.noreply.github.com>
2024-10-24 20:04:30 -0500

    waypoints

---
+++ data/lib/additional/waypoints_lib.lua
@@ -0,0 +1,26 @@
+waypointList = {
+    [1] = {
+        name = "zone 1",
+        actionId = 61199,
+        storage = "61199",
+        effect = 5,
+        gold = 1,
+        position = Position(283, 756, 7),
+        requireQuest = { check = false, storage = 5000, value = 5000 },
+        cost = {
+            { id = 2160, count = 5 },
+        },
+    },
+    [2] = {
+        name = "zone 2",
+        actionId = 61200,
+        storage = "61200",
+        effect = 6,
+        gold = 2,
+        position = Position(469, 730, 5),
+        requireQuest = { check = false, storage = 5000, value = 5000 },
+        cost = {
+            { id = 2160, count = 10 },
+        },
+    },
+}
