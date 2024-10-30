6574415a85a89abba184fdb45c069b6308b4a69a
Nokturno <35547755+nok1111@users.noreply.github.com>
2024-10-30 12:54:37 -0500

    Update spell_info.lua

--- data/lib/additional/spell_info.lua
+++ data/lib/additional/spell_info.lua
@@ -3,115 +3,117 @@ ultimateSpellLimit = 1
 
 SpellBooks = {
 	  -- all common spells
-    [36835] = {spellName = "Rend", spellType = "common", storageId = 20001},
-    [1964] = {spellName = "mad", spellType = "common", storageId = 20002},
-    [1965] = {spellName = "pingpong", spellType = "common", storageId = 20003},
-    [1966] = {spellName = "cage", spellType = "common", storageId = 20004},
-    [1967] = {spellName = "explo", spellType = "common", storageId = 20005},
-    [1968] = {spellName = "double", spellType = "common", storageId = 20006},
-    [1969] = {spellName = "anihilation", spellType = "common", storageId = 20007},
-    [1970] = {spellName = "mass ani", spellType = "common", storageId = 20008},
-    [1971] = {spellName = "mass ani mon", spellType = "common", storageId = 20009},
+    [36835] = {spellName = "Rend", spellType = "common", storageId = 20001},--
+    [39768] = {spellName = "Magic Shield", spellType = "common", storageId = 20002},--
+    [39797] = {spellName = "Bouncing Sphere", spellType = "common", storageId = 20003},--
+    [00000] = {spellName = "cage", spellType = "common", storageId = 20004},-- missing
+    [1967] = {spellName = "explo", spellType = "common", storageId = 20005},--missing
+    [1968] = {spellName = "double", spellType = "common", storageId = 20006},--missing
+    [1969] = {spellName = "anihilation", spellType = "common", storageId = 20007},--missing
+    [1970] = {spellName = "mass ani", spellType = "common", storageId = 20008},--missing
+    [1971] = {spellName = "mass ani mon", spellType = "common", storageId = 20009},--missing
     [1972] = {spellName = "push", spellType = "common", storageId = 20010},
     [1973] = {spellName = "flat", spellType = "common", storageId = 20011},
-    [1974] = {spellName = "pull", spellType = "common", storageId = 20012},
-    [1975] = {spellName = "Brutal Swing", spellType = "common", storageId = 20013},
-    [1976] = {spellName = "Challenging Spear", spellType = "common", storageId = 20014},
-    [1977] = {spellName = "Fire Within", spellType = "common", storageId = 20015},
-    [1978] = {spellName = "Charge", spellType = "common", storageId = 20016},
-    [1979] = {spellName = "whirlwind", spellType = "common", storageId = 20017},
-    [1980] = {spellName = "Roar", spellType = "common", storageId = 20018},
-    [1981] = {spellName = "Phoenix Wrath", spellType = "common", storageId = 20019},
-    [1982] = {spellName = "Dragon Aura", spellType = "common", storageId = 20020},
+    [39816] = {spellName = "pull", spellType = "common", storageId = 20012},--
+    [39821] = {spellName = "Brutal Swing", spellType = "common", storageId = 20013},--
+    [39836] = {spellName = "Challenging Spear", spellType = "common", storageId = 20014},--
+    [39823] = {spellName = "Fire Within", spellType = "common", storageId = 20015},--
+    [39822] = {spellName = "Charge", spellType = "common", storageId = 20016},--
+    [39818] = {spellName = "whirlwind", spellType = "common", storageId = 20017},--
+    [00000] = {spellName = "Roar", spellType = "common", storageId = 20018},--
+    [39819] = {spellName = "Phoenix Wrath", spellType = "common", storageId = 20019},--
+    [39820] = {spellName = "Dragon Aura", spellType = "common", storageId = 20020},--
     [1983] = {spellName = "Dragons Call", spellType = "common", storageId = 20021},
-    [1984] = {spellName = "Death Shrine", spellType = "common", storageId = 20022},
+    [39814] = {spellName = "Death Shrine", spellType = "common", storageId = 20022},--
     [1985] = {spellName = "Divine Punishment", spellType = "common", storageId = 20023},
     [1986] = {spellName = "Penitence", spellType = "common", storageId = 20024},
-    [1987] = {spellName = "Holy Ground", spellType = "common", storageId = 20025},
-    [1988] = {spellName = "Sacred Ground", spellType = "common", storageId = 20026},
-    [1989] = {spellName = "Crusader Strike", spellType = "common", storageId = 20027},
+    [39827] = {spellName = "Holy Ground", spellType = "common", storageId = 20025},--
+    [39830] = {spellName = "Sacred Ground", spellType = "common", storageId = 20026},--
+    [39828] = {spellName = "Crusader Strike", spellType = "common", storageId = 20027},--
     [1990] = {spellName = "Divine Storm", spellType = "common", storageId = 20028},
-    [1991] = {spellName = "Exorcism", spellType = "common", storageId = 20029},
+    [39796] = {spellName = "Exorcism", spellType = "common", storageId = 20029},--
     [1992] = {spellName = "Divine Force", spellType = "common", storageId = 20030},
     [1993] = {spellName = "Smite", spellType = "common", storageId = 20031},
     [1994] = {spellName = "Light Beam", spellType = "common", storageId = 20032},
-    [1995] = {spellName = "Summon Guardian of Light", spellType = "common", storageId = 20033},
-    [1996] = {spellName = "Kings Blessing", spellType = "common", storageId = 20034},
-    [1997] = {spellName = "Living Bomb", spellType = "common", storageId = 20035},
+    [39873] = {spellName = "Summon Guardian of Light", spellType = "common", storageId = 20033},--
+    [39874] = {spellName = "Kings Blessing", spellType = "common", storageId = 20034},--
+    [39799] = {spellName = "Living Bomb", spellType = "common", storageId = 20035},--
     [1998] = {spellName = "Energy Blast", spellType = "common", storageId = 20036},
-    [1999] = {spellName = "Fire Blast", spellType = "common", storageId = 20037},
-    [2000] = {spellName = "Elemental Blast", spellType = "common", storageId = 20038},
-    [2001] = {spellName = "Ice Nova", spellType = "common", storageId = 20039},
-    [2002] = {spellName = "Mana Distortion", spellType = "common", storageId = 20040},
-    [2003] = {spellName = "Mana Flow", spellType = "common", storageId = 20041},
-    [2004] = {spellName = "Hand of God", spellType = "common", storageId = 20042},
-    [2005] = {spellName = "Frost Wave", spellType = "common", storageId = 20043},
-    [2006] = {spellName = "Arcane Missiles", spellType = "common", storageId = 20044},
-    [2007] = {spellName = "Teleport", spellType = "common", storageId = 20045},
-    [2008] = {spellName = "Blizzard", spellType = "common", storageId = 20046},
-    [2009] = {spellName = "Hells Core", spellType = "common", storageId = 20047},
-    [2010] = {spellName = "Void Recall", spellType = "common", storageId = 20048},
+    [39784] = {spellName = "Fire Blast", spellType = "common", storageId = 20037},--
+    [39781] = {spellName = "Elemental Blast", spellType = "common", storageId = 20038},--
+    [39776] = {spellName = "Ice Nova", spellType = "common", storageId = 20039},--
+    [39873] = {spellName = "Mana Distortion", spellType = "common", storageId = 20040},--
+    [39772] = {spellName = "Mana Flow", spellType = "common", storageId = 20041},--
+    [39788] = {spellName = "Hand of God", spellType = "common", storageId = 20042},--
+    [39778] = {spellName = "Frost Wave", spellType = "common", storageId = 20043},--
+    [39774] = {spellName = "Arcane Missiles", spellType = "common", storageId = 20044},--
+    [39786] = {spellName = "Teleport", spellType = "common", storageId = 20045},--
+    [39789] = {spellName = "Blizzard", spellType = "common", storageId = 20046},--
+    [39779] = {spellName = "Hells Core", spellType = "common", storageId = 20047},--ultimate
+    [39802] = {spellName = "Void Recall", spellType = "common", storageId = 20048},--
     [2011] = {spellName = "Zombie Wall", spellType = "common", storageId = 20049},
-    [2012] = {spellName = "Curse", spellType = "common", storageId = 20050},
-    [2013] = {spellName = "Fear", spellType = "common", storageId = 20051},
-    [2014] = {spellName = "Shadow Strike", spellType = "common", storageId = 20052},
-    [2015] = {spellName = "Summon Void Healer", spellType = "common", storageId = 20053},
-    [2016] = {spellName = "Blood Pact", spellType = "common", storageId = 20054},
-    [2017] = {spellName = "Party Vitality", spellType = "common", storageId = 20055},
+    [39854] = {spellName = "Curse", spellType = "common", storageId = 20050},--
+    [39855] = {spellName = "Fear", spellType = "common", storageId = 20051},--
+    [39834] = {spellName = "Shadow Strike", spellType = "common", storageId = 20052},--
+    [00000] = {spellName = "Summon Void Healer", spellType = "common", storageId = 20053},
+    [39883] = {spellName = "Blood Pact", spellType = "common", storageId = 20054},--
+    [39882] = {spellName = "Party Vitality", spellType = "common", storageId = 20055},--
     [2018] = {spellName = "Summon Void Guard", spellType = "common", storageId = 20056},
     [2019] = {spellName = "Summon Void Archer", spellType = "common", storageId = 20057},
     [2020] = {spellName = "Blood Wall", spellType = "common", storageId = 20058},
     [2021] = {spellName = "Dark Plague", spellType = "common", storageId = 20059},
-    [2022] = {spellName = "Void Creep", spellType = "common", storageId = 20060},
-    [2023] = {spellName = "Soul Rain", spellType = "common", storageId = 20061},   
+    [39853] = {spellName = "Void Creep", spellType = "common", storageId = 20060},--
+    [39850] = {spellName = "Soul Rain", spellType = "common", storageId = 20061}, --  
 	
-	 [20062] = {spellName = "Stealth", spellType = "common", storageId = 20062},
+	[39847] = {spellName = "Stealth", spellType = "common", storageId = 20062},--
     [20063] = {spellName = "Mutilate", spellType = "common", storageId = 20063},
-    [20064] = {spellName = "Shadow Hunt", spellType = "common", storageId = 20064},
-    [20065] = {spellName = "Shadowstep", spellType = "common", storageId = 20065},
-    [20066] = {spellName = "Backstab", spellType = "common", storageId = 20066},
-    [20067] = {spellName = "Dark Ambush", spellType = "common", storageId = 20067},
-    [20068] = {spellName = "Fan of Knives", spellType = "common", storageId = 20068},
+    [39857] = {spellName = "Shadow Hunt", spellType = "common", storageId = 20064},--
+    [39848] = {spellName = "Shadowstep", spellType = "common", storageId = 20065},--
+    [39844] = {spellName = "Backstab", spellType = "common", storageId = 20066},--
+    [39837] = {spellName = "Dark Ambush", spellType = "common", storageId = 20067},--ultimate
+    [39840] = {spellName = "Fan of Knives", spellType = "common", storageId = 20068},--
     [20069] = {spellName = "Shadow Form", spellType = "common", storageId = 20069},
     [20070] = {spellName = "Dark Rupture", spellType = "common", storageId = 20070},
-    [20071] = {spellName = "Void Execution", spellType = "common", storageId = 20071},
-    [20072] = {spellName = "Blood Blades", spellType = "common", storageId = 20072},
-    [20073] = {spellName = "Cosmic Force", spellType = "common", storageId = 20073},
-    [20074] = {spellName = "Regrowth", spellType = "common", storageId = 20074},
+    [39771] = {spellName = "Void Execution", spellType = "common", storageId = 20071},--
+    [39843] = {spellName = "Blood Blades", spellType = "common", storageId = 20072},--
+    [39825] = {spellName = "Cosmic Force", spellType = "common", storageId = 20073},--
+    [39868] = {spellName = "Regrowth", spellType = "common", storageId = 20074},--
     [20075] = {spellName = "Aery Wrath", spellType = "common", storageId = 20075},
     [20076] = {spellName = "Star Fall", spellType = "common", storageId = 20076},
-    [20077] = {spellName = "Falling Star", spellType = "common", storageId = 20077},
-    [20078] = {spellName = "Rain fall", spellType = "common", storageId = 20078},
-    [20079] = {spellName = "Heal Party", spellType = "common", storageId = 20079},
-    [20080] = {spellName = "Grow", spellType = "common", storageId = 20080},
+    [39876] = {spellName = "Falling Star", spellType = "common", storageId = 20077},--
+    [39826] = {spellName = "Rain fall", spellType = "common", storageId = 20078},--
+    [20079] = {spellName = "Heal Party", spellType = "common", storageId = 20079},--missing will change
+    [39863] = {spellName = "Grow", spellType = "common", storageId = 20080},--
     [20081] = {spellName = "Lunar Beam", spellType = "common", storageId = 20081},
     [20082] = {spellName = "Aery Strikes", spellType = "common", storageId = 20082},
-    [20083] = {spellName = "Full Moon", spellType = "common", storageId = 20083},
-    [20084] = {spellName = "Soul Fists", spellType = "common", storageId = 20084},
-    [20085] = {spellName = "Soul Barrage", spellType = "common", storageId = 20085},
-    [20086] = {spellName = "Anger Release", spellType = "common", storageId = 20086},
+    [39869] = {spellName = "Full Moon", spellType = "common", storageId = 20083},--ultimate
+    [39811] = {spellName = "Soul Fists", spellType = "common", storageId = 20084},--
+    [39809] = {spellName = "Soul Barrage", spellType = "common", storageId = 20085},--
+    [39810] = {spellName = "Anger Release", spellType = "common", storageId = 20086},--
     [20087] = {spellName = "Spectral Wave", spellType = "common", storageId = 20087},
     [20088] = {spellName = "Underworld Gaze", spellType = "common", storageId = 20088},
-    [20089] = {spellName = "Hollow Blade", spellType = "common", storageId = 20089},
-    [20090] = {spellName = "Veil of Anguish", spellType = "common", storageId = 20090},
-    [20091] = {spellName = "Absolute Denial", spellType = "common", storageId = 20091},
-    [20092] = {spellName = "Soul Link", spellType = "common", storageId = 20092},
+    [39812] = {spellName = "Hollow Blade", spellType = "common", storageId = 20089},--
+    [39867] = {spellName = "Veil of Anguish", spellType = "common", storageId = 20090},--
+    [39787] = {spellName = "Absolute Denial", spellType = "common", storageId = 20091},--
+    [39813] = {spellName = "Soul Link", spellType = "common", storageId = 20092},--
     [20093] = {spellName = "Drain Soul", spellType = "common", storageId = 20093},
-    [20094] = {spellName = "Void Slumber", spellType = "common", storageId = 20094},
-    [20095] = {spellName = "Terra Strike", spellType = "common", storageId = 20095},
-    [20096] = {spellName = "Living Seeds", spellType = "common", storageId = 20096},
-    [20097] = {spellName = "Life Bloom", spellType = "common", storageId = 20097},
-    [20098] = {spellName = "Rejuvenation", spellType = "common", storageId = 20098},
-    [20099] = {spellName = "Seed Germination", spellType = "common", storageId = 20099},
-    [20100] = {spellName = "Travel Form", spellType = "common", storageId = 20100},
-    [20101] = {spellName = "Carnivorous Vile", spellType = "common", storageId = 20101},
-    [20102] = {spellName = "living ground", spellType = "common", storageId = 20102},
+    [20094] = {spellName = "Void Slumber", spellType = "common", storageId = 20094},--change later
+    [39792] = {spellName = "Terra Strike", spellType = "common", storageId = 20095},--
+    [39801] = {spellName = "Living Seeds", spellType = "common", storageId = 20096},--
+    [39800] = {spellName = "Life Bloom", spellType = "common", storageId = 20097},--
+    [39868] = {spellName = "Rejuvenation", spellType = "common", storageId = 20098},--
+    [00000] = {spellName = "Seed Germination", spellType = "common", storageId = 20099},
+    [00000] = {spellName = "Travel Form", spellType = "common", storageId = 20100},
+    [39803] = {spellName = "Carnivorous Vile", spellType = "common", storageId = 20101},--
+    [39807] = {spellName = "living ground", spellType = "common", storageId = 20102},--
     [20103] = {spellName = "Thorns", spellType = "common", storageId = 20103},
-    [20104] = {spellName = "Piercing Wave", spellType = "common", storageId = 20104},
-    [20105] = {spellName = "Bear Form", spellType = "common", storageId = 20105},
-    [20106] = {spellName = "Bless of the Forest", spellType = "common", storageId = 20106},
-    [20107] = {spellName = "Wrath of Nature", spellType = "common", storageId = 20107},
+    [39791] = {spellName = "Piercing Wave", spellType = "common", storageId = 20104},--
+    [39871] = {spellName = "Bear Form", spellType = "common", storageId = 20105},--
+    [39864] = {spellName = "Bless of the Forest", spellType = "common", storageId = 20106},--
+    [39870] = {spellName = "Wrath of Nature", spellType = "common", storageId = 20107},--
+
     [20108] = {spellName = "Soul Form", spellType = "common", storageId = 20108},
+
     [20109] = {spellName = "Earth Dance", spellType = "common", storageId = 20109},
     [20110] = {spellName = "Charged Strike", spellType = "common", storageId = 20110},
     [20111] = {spellName = "Static Charge", spellType = "common", storageId = 20111},
@@ -120,19 +122,20 @@ SpellBooks = {
     [20114] = {spellName = "Swift Elusion", spellType = "common", storageId = 20114},
     [20115] = {spellName = "Chain of Lightning", spellType = "common", storageId = 20115},
     [20116] = {spellName = "Lightning Spear", spellType = "common", storageId = 20116},
-    [20117] = {spellName = "Overcharge", spellType = "common", storageId = 20117},
-    [20118] = {spellName = "Magnetic Field", spellType = "common", storageId = 20118},
-    [20119] = {spellName = "Magnetic Orb", spellType = "common", storageId = 20119},
-    [20120] = {spellName = "Flaming Shot", spellType = "common", storageId = 20120},
-    [20121] = {spellName = "Beer Barrel", spellType = "common", storageId = 20121},
-    [20122] = {spellName = "Explosive Shots", spellType = "common", storageId = 20122},
-    [20123] = {spellName = "Wind Barrel", spellType = "common", storageId = 20123},
-    [20124] = {spellName = "Condemn Shot", spellType = "common", storageId = 20124},
+    [39806] = {spellName = "Overcharge", spellType = "common", storageId = 20117},--
+    [39790] = {spellName = "Magnetic Field", spellType = "common", storageId = 20118},--
+    [39785] = {spellName = "Magnetic Orb", spellType = "common", storageId = 20119},--
+
+    [39881] = {spellName = "Flaming Shot", spellType = "common", storageId = 20120},--ga
+    [39839] = {spellName = "Beer Barrel", spellType = "common", storageId = 20121},--
+    [39849] = {spellName = "Explosive Shots", spellType = "common", storageId = 20122},--
+    [39794] = {spellName = "Wind Barrel", spellType = "common", storageId = 20123},--
+    [39845] = {spellName = "Condemn Shot", spellType = "common", storageId = 20124},--
     [20125] = {spellName = "Nail Bomb", spellType = "common", storageId = 20125},
-    [20126] = {spellName = "Healing Barrel", spellType = "common", storageId = 20126},
-    [20127] = {spellName = "Arrow Rain", spellType = "common", storageId = 20127},
-    [20128] = {spellName = "Frost Barrel", spellType = "common", storageId = 20128},
-    [20129] = {spellName = "Destructive Shot", spellType = "common", storageId = 20129},
+    [39805] = {spellName = "Healing Barrel", spellType = "common", storageId = 20126},--
+    [39838] = {spellName = "Arrow Rain", spellType = "common", storageId = 20127},--
+    [39804] = {spellName = "Frost Barrel", spellType = "common", storageId = 20128},--
+    [39880] = {spellName = "Destructive Shot", spellType = "common", storageId = 20129},-- ultimate
     [20130] = {spellName = "Aspect Mastery", spellType = "common", storageId = 20130},
     
 	
