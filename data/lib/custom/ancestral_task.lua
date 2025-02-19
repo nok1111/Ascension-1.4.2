091c6262fb80e1762a5d998c3537e7a694990eae
Nokturno <35547755+nok1111@users.noreply.github.com>
2025-02-18 20:31:21 -0600

    maybe not needed

---
+++ data/lib/custom/ancestral_task.lua
@@ -0,0 +1,172 @@
+task_monsters = {
+    [1] = {name = "raging demons", mons_list = {"demon"}, storage = 400029, amount = 115, exp = 6500000, pointsTask = {4, 4}, items = {{id = 2472, count = 1},{id = 2470, count = 1}, {id = 7393, count = 1}, {id = 2160, count = 10} }, monsters = "Kill Demons around the world"},
+    [2] = {name = "fire dragons", mons_list = {"dragon", "dragon lord"}, storage = 400030, amount = 200, exp = 4000000, pointsTask = {2, 2}, items = {{id = 7399, count = 1}, {id = 7402, count = 1}, {id = 2160, count = 4 }}, monsters = "Kill Dragons and Dragon lords around the world"},
+    [3] = {name = "silence mountain", mons_list = {"frazzlemaw", "silencer"}, storage = 400031, amount = 150, exp = 7500000, pointsTask = {3, 2}, items = {{id = 22396, count = 1}, {id = 2160, count = 10}, {id = 18429, count = 10}, {id = 18421, count = 10}}, monsters = "Kill Frazzlemaws and Silencers"},
+    [4] = {name = "fear valley", mons_list = {"retching horror", "choking Fear"}, storage = 400032, amount = 150, exp = 8300000, pointsTask = {4, 4}, items = {{id = 22396, count = 2}, {id = 2645, count = 1}, {id = 7412, count = 1}, {id = 7451, count = 1}, {id = 2160, count = 20}}, monsters = "Kill Retching Horrors and Choking Fear"},
+    [5] = {name = "poison combo", mons_list = {"hydra", "serpent spawn"}, storage = 400033, amount = 150, exp = 5000000, pointsTask = {3, 2}, items = {{id = 2195, count = 1}, {id = 8880, count = 1}, {id = 2498, count = 1}, {id = 11230, count = 8}, {id = 11199, count = 8}, {id = 2160, count = 6}}, monsters = "Kill Hydras and Serpent Spawn"},
+    [6] = {name = "night reapers", mons_list = {"grim reaper", "nightmare"}, storage = 400034, amount = 150, exp = 6150000, pointsTask = {3, 2}, items = {{id = 2160, count = 6}, {id = 8889, count = 5}, {id = 6526, count = 1}, {id = 6500, count = 10}}, monsters = "Kill Grim Reapers and nightmares"},
+    [7] = {name = "catacombs", mons_list = {"demon", "dark torturer", "juggernaut", "hellhound", "destroyer", "hellspawn"}, storage = 400035, amount = 250, exp = 18000000, pointsTask = {6, 6}, items = {{id = 2160, count = 30}, {id = 7421, count = 1}, {id = 5801, count = 1}, {id = 2136, count = 1}, {id = 5741, count = 1}, {id = 9970, count = 10}, {id = 2150, count = 10}, {id = 2147, count = 10}}, monsters = "Kill Demons, Hellhounds, Juggernaut, Destroyers, Hellspawn and Dark Torturers"},
+    [8] = {name = "wyrms", mons_list = {"wyrm", "elder wyrm"}, storage = 400036, amount = 150, exp = 2375000, pointsTask = {2, 2}, items = {{id = 2160, count = 5},{id = 8855, count = 1}, {id = 7451, count = 1}, {id = 2145, count = 10}}, monsters = "Kill Wyrms and Elder Wyrms"},
+    [9] = {name = "minotaurs", mons_list = {"worm priestess", "minotaur amazon", "minotaur hunter", "mooh'tah warrior"}, storage = 400037, amount = 150, exp = 800000, pointsTask = {3, 2}, items = {{id = 2160, count = 5},{id = 5878, count = 30}, {id = 23537, count = 1}, {id = 7401, count = 1}}, monsters = "Kill Wormd Priestess, Minotaur Amazon, Hunter and Mooh'tah warrior"},
+    [10] = {name = "devourers", mons_list = {"stone devourer", "lost berserker", "magma crawler"}, storage = 400038, amount = 150, exp = 800000, pointsTask = {3, 2}, items = {{id = 2160, count = 8},{id = 2445, count = 1}, {id = 18409, count = 1}, {id = 5904, count = 2}}, monsters = "Kill Stone Devourers, Lost Berserkers and Magma Crawlers"},
+    [11] = {name = "depth demons", mons_list = {"alpha demon", "rhino of the depths", "skullcrusher demon", "demon of the depths"}, storage = 400039, amount = 150, exp = 14500000, pointsTask = {4, 3}, items = {{id = 26780, count = 4},{id = 26779, count = 2}}, monsters = "Kill Alpha Demons, Skullcrusher Demons, Rhino of the depths and Demon of the Depths on Azure Portals."},
+    [12] = {name = "portal azure", mons_list = {"aberration of the depths", "azure guard", "black dragon", "brontosaurus", "butcher ogre", "colossal ice monkey", "giant hornet", "netheria dragon", "reflect spider", "ruthless spider", "venom spider"}, storage = 400040, amount = 300, exp = 30000000, pointsTask = {7, 4}, items = {{id = 26779, count = 4},{id = 26778, count = 3}, {id = 26780, count = 4},}, monsters = "Kill Aberration of the depths, azure guards, black dragons, brontosaurus, butcher ogre, colossal ice monkey, giant hornet, netheria dragon, reflect spider, ruthless spider and venom spider on Azure Portals."},
+    [13] = {name = "darklight", mons_list = {"bloated man-maggot", "converter", "darklight construct", "darklight emitter", "darklight matter", "darklight source", "darklight striker", "elder bloodjaw", "meandering mushroom", "mycobiontic beetle", "oozing carcass", "oozing corpus", "rotten man-maggot", "sopping carcass", "walking pillar", "wandering pillar"}, storage = 400041, amount = 350, exp = 55000000, pointsTask = {10, 6}, items = {{id = 28058, count = 1},{id = 27990, count = 1}}, monsters = "Kill all monsters on the Bakragore Area on Kilmaresh."},
+    [14] = {name = "darklight", mons_list = {"bloated man-maggot", "converter", "darklight construct", "darklight emitter", "darklight matter", "darklight source", "darklight striker", "elder bloodjaw", "meandering mushroom", "mycobiontic beetle", "oozing carcass", "oozing corpus", "rotten man-maggot", "sopping carcass", "walking pillar", "wandering pillar"}, storage = 400042, amount = 350, exp = 78000000, pointsTask = {10, 6}, items = {{id = 28058, count = 1},{id = 27990, count = 1}}, monsters = "Kill all monsters on the Bakragore Area on Kilmaresh."},
+    [15] = {name = "secret library", mons_list = {"animated feather", "biting book", "brain squid", "burning book", "cursed book", "energetic book", "energuardian of tales", "floating savant", "flying book", "guardian of tales", "icecold book", "ink blob", "knowledge elemental", "rage squid", "squid warden"}, storage = 400043, amount = 350, exp = 70000000, pointsTask = {10, 6}, items = {{id = 28058, count = 1},{id = 27990, count = 1}}, monsters = "Kill all monsters on Secret Library Area."},
+    [16] = {name = "soulwar", mons_list = {"bony sea evil", "brachiodemon", "branchy crawler", "capricious phantom", "cloak of terror", "courage leech", "distorted phantom", "druid's apparition", "hazardous phantom", "infernal demon", "infernal phantom", "knight's apparition", "many faces", "mould phantom", "paladin's apparition", "rotten golem", "sorcerer's apparition", "turbulent elemental", "vibrant phantom"}, storage = 400044, amount = 350, exp = 70000000, pointsTask = {10, 6}, items = {{id = 28058, count = 1},{id = 27990, count = 1}, {id = 26778, count = 3}, {id = 27894, count = 8}}, monsters = "Kill all monsters Soulwar Area."},
+   }
+ 
+ task_daily = {
+    [1] = {name = "plagirath", mons_list = {"plagirath"}, storage = 503001, amount = 1, exp = 18500000, pointsTask = {5, 4}, items = {{id = 2160, count = 12}}, monsters = "Kill Plagirath."},
+    [2] = {name = "ushuriel", mons_list = {"ushuriel"}, storage = 503002, amount = 1, exp = 9000000, pointsTask = {3, 2}, items = {{id = 2160, count = 4}}, monsters = "Kill Ushuriel."},
+    [3] = {name = "abyssador", mons_list = {"abyssador"}, storage = 503003, amount = 1, exp = 18500000, pointsTask = {6, 4}, items = {{id = 2160, count = 12}}, monsters = "Kill Abyssador."},
+    [4] = {name = "gaz'haragoth", mons_list = {"gaz'haragoth"}, storage = 503004, amount = 1, exp = 45000000, pointsTask = {10, 8}, items = {{id = 22397, count = 3}, {id = 22396, count = 10}}, monsters = "Kill Gaz'haragoth."},
+    [5] = {name = "bakragore", mons_list = {"bakragore"}, storage = 503005, amount = 1, exp = 30000000, pointsTask = {8, 6}, items = {{id = 27418, count = 1},  {id = 27419, count = 1}, {id = 27420, count = 1},  {id = 27421, count = 1}}, monsters = "Kill Bakragore on Kilmaresh."},
+    [6] = {name = "prince skeletal", mons_list = {"prince skeletal"}, storage = 503006, amount = 1, exp = 10000000, pointsTask = {5, 4}, items = {{id = 2160, count = 12}}, monsters = "Kill Prince Skeletal on Bones Dungeon."},
+    [7] = {name = "azure", mons_list = {"azure"}, storage = 503007, amount = 1, exp = 30000000, pointsTask = {8, 6}, items = {{id = 26780, count = 10}}, monsters = "Kill Azure on Azure Portals."},
+    [8] = {name = "astro king", mons_list = {"astro king"}, storage = 503008, amount = 1, exp = 65000000, pointsTask = {10, 8}, items = {{id = 28033, count = 2}}, monsters = "Kill Astro King on Astro Quest."},
+    [9] = {name = "arodis saron", mons_list = {"arodis saron"}, storage = 503009, amount = 1, exp = 65000000, pointsTask = {10, 8}, items = {{id = 28033, count = 2}}, monsters = "Kill Arodis Saron on Arodis Quest."},
+    [10] = {name = "mercurial mage", mons_list = {"mercurial mage"}, storage = 503010, amount = 1, exp = 65000000, pointsTask = {10, 8}, items = {{id = 28033, count = 2}}, monsters = "Kill Mercurial Mage on Mercurial Quest."},
+    [11] = {name = "goshnar's megalomania", mons_list = {"goshnar's megalomania"}, storage = 503011, amount = 1, exp = 30000000, pointsTask = {8, 4}, items = {{id = 27344, count = 4}, {id = 27345, count = 1},  {id = 27346, count = 1}}, monsters = "Kill Goshnar's Megalomania on Kilmaresh."},
+} 
+
+ 
+ task_storage = 480000
+ task_sto_time = 480002
+ task_time = 1
+ taskd_storage = 480004
+ time_daySto = 480005
+ 
+ 
+ local ranks_task = {
+ [{1, 40}] = "Rookie", 
+ [{41, 100}] = "Bronze",
+ [{101, 250}] = "Silver",
+ [{251, 470}] = "Gold",
+ [{471, 770}] = "Platinum",
+ [{771, 1220}] = "Diamond",
+ [{1221, 1820}] = "Ancestral",
+ [{1821, 2820}] = "Mystic",
+ [{2821, 4320}] = "Abyssal",
+ [{4321, 6320}] = "Ascending",
+ [{6321, 9999}] = "Chaos",
+ [{10000, math.huge}] = "Awakening"
+ }
+ 
+ local RankSequence = {
+ ["Rookie"] = 1,
+ ["Bronze"] = 2,
+ ["Silver"] = 3,
+ ["Gold"] = 4,
+ ["Platinum"] = 5,
+ ["Diamond"] = 6,
+ ["Ancestral"] = 7,
+ ["Mystic"] = 8,
+ ["Abyssal"] = 9,
+ ["Ascending"] = 10,
+ ["Chaos"] = 11,
+ ["Awakening"] = 12
+ }
+ 
+ function rankIsEqualOrHigher(myRank, RankCheck)
+     local ret_1 = RankSequence[myRank]
+     local ret_2 = RankSequence[RankCheck]
+     return ret_1 >= ret_2
+ end
+ 
+ function getTaskInfos(player)
+    local player = Player(player)
+    local taskInfo = task_monsters[player:getStorageValue(task_storage)]
+    if taskInfo then
+        taskInfo.taskType = "Normal"
+    end
+    return taskInfo or false
+end
+
+function getTaskDailyInfo(player)
+    local player = Player(player)
+    local taskInfo = task_daily[player:getStorageValue(taskd_storage)]
+    if taskInfo then
+        taskInfo.taskType = "Daily"
+    end
+    return taskInfo or false
+end
+ 
+ 
+ function taskPoints_get(player)
+    local points = 0
+    local accountId = player:getGuid() 
+    local resultId = db.storeQuery("SELECT `ancestral_points` FROM `players` WHERE `id` = " .. accountId)
+    if resultId ~= false then
+        points = result.getDataInt(resultId, "ancestral_points")
+        result.free(resultId)
+    end
+    return points
+ end
+
+
+
+ function taskPoints_add(player, count)
+    local accountId = player:getGuid() 
+
+    db.query(string.format("UPDATE players SET ancestral_points = ancestral_points + %d WHERE id = %d", count, accountId))
+end
+ 
+ function taskPoints_remove(player, count)
+    local taskPoints = taskPoints_get(player) or 0 
+    local accountId = player:getGuid() 
+
+    local newPoints = taskPoints - count
+    if newPoints < 0 then
+        newPoints = 0
+    end
+
+    db.query(string.format("UPDATE players SET ancestral_points = %d WHERE id = %d", newPoints, accountId))
+end
+
+
+ 
+ function taskRank_get(player)
+    local accountId = player:getGuid() 
+    local points = 0
+    local resultId = db.storeQuery("SELECT `ancestral_rank_points` FROM `players` WHERE `id` = " .. accountId)
+    if resultId ~= false then
+        points = result.getDataInt(resultId, "ancestral_rank_points")
+        result.free(resultId)
+    end
+    return points
+ end
+ 
+ function taskRank_add(player, count)
+    local accountId = player:getGuid() 
+
+    db.query(string.format("UPDATE players SET ancestral_rank_points = ancestral_rank_points + %d WHERE id = %d", count, accountId))
+end
+ 
+ function getRankTask(player)
+     local pontos = taskRank_get(player)
+     local ret
+     for _, z in pairs(ranks_task) do
+         if pontos >= _[1] and pontos <= _[2] then
+             ret = z
+         end
+     end
+     return ret
+ end
+ 
+ function getItemsFromTable(itemtable)
+      local text = ""
+      for v = 1, #itemtable do
+          count, info = itemtable[v].count, ItemType(itemtable[v].id)
+          local ret = "\n   - "
+          if v == 1 then
+              ret = ""
+          elseif v == #itemtable then
+              ret = "\n   - "
+          end
+          text = text .. ret
+          text = text .. (count > 1 and count or info:getArticle()).." "..(count > 1 and info:getPluralName() or info:getName())
+      end
+      return text
+ end
