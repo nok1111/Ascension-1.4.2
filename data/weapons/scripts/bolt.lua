bef745322c43638522289af093a916d1184e430e
Nokturno <35547755+nok1111@users.noreply.github.com>
2025-08-07 11:50:50 -0500

    bows cleanup

--- data/weapons/scripts/bolt.lua
+++ data/weapons/scripts/bolt.lua
@@ -6,9 +6,7 @@ combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, 24)
 
 
 function onGetFormulaValues(player, skill, attack, factor)
-local distancefactor = skill
-
-	local damage = (player:getLevel() / 5) + ((distancefactor * attack) * 0.085) 
+	local damage = (player:getLevel() / 5) + ((skill * attack) * 0.085) 
 	return -damage
 end
 combat:setCallback(CALLBACK_PARAM_SKILLVALUE, "onGetFormulaValues")
@@ -17,7 +15,6 @@ combat:setCallback(CALLBACK_PARAM_SKILLVALUE, "onGetFormulaValues")
 
 
 function onUseWeapon(player, variant, creature)
-    print("bolt")
 	if not combat:execute(player, variant) then
 		return false
 	end
